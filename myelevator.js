e92381722c7d1fb106e15bb0e50de52ab17a3247
danielistyo <danielistyo16@gmail.com>
2018-01-23 23:46:49 +0700

    Optimize logic

--- myelevator.js
+++ myelevator.js
@@ -41,39 +41,71 @@
  * building.get_num_floors()
  */
 
-Elevator.prototype.decide = function() {
+Elevator.prototype.decide = function () {
     var simulator = Simulator.get_instance();
     var building = simulator.get_building();
     var num_floors = building.get_num_floors();
     var elevators = Simulator.get_instance().get_building().get_elevator_system().get_elevators();
     var time_counter = simulator.get_time_counter();
     var requests = simulator.get_requests();
-    
+
     var elevator = this;
+    // get people inside elevator
     var people = this.get_people();
-    var person = people.length > 0 ? people[0] : undefined;
-    
-    if(elevator) {
-        elevator.at_floor();
-        elevator.get_destination_floor();
-        elevator.get_position();
+
+    if (elevator) {
+        var elevatorCurrentFloor = elevator.at_floor();
+        var elevatordestinationFloor = elevator.get_destination_floor();
+        var elevatorPosition = elevator.get_position();
     }
-    
-    if(person) {
+
+    // **************SERVE PERSON**************
+    // find nearest person destination floor
+    var person = people.length > 0 ? people[0] : undefined;
+    if (person) {
+        for (var i = 0; i < people.length; i++) {
+            if (Math.abs(people[i].get_destination_floor() - elevator.at_floor()) <
+                Math.abs(person.get_destination_floor() - elevator.at_floor())) {
+                person = people[i];
+            }
+        }
         person.get_floor();
+        // serve person. go to destination floor 
+        // reorder 
         return this.commit_decision(person.get_destination_floor());
     }
-    
-    for(var i = 0;i < requests.length;i++) {
+
+
+    // **************SERVE REQUEST**************
+    for (var i = 0; i < requests.length; i++) {
         var handled = false;
-        for(var j = 0;j < elevators.length;j++) {
-            if(elevators[j].get_destination_floor() == requests[i]) {
+
+        // find floor request that haven't been handled yet
+        for (var j = 0; j < elevators.length; j++) {
+            var elev = elevators[j];
+
+            var req = requests[i];
+            // if has been handled, break, go to next request
+            if (elev.get_destination_floor() == req) {
                 handled = true;
                 break;
             }
         }
-        if(!handled) {
-            return this.commit_decision(requests[i]);
+
+        // let handle unhandled request
+        if (!handled) {
+            var nearestElev = elevator;
+            // find nearest elevator to take the person
+            for (var j = 0; j < elevators.length; j++) {
+                var elev = elevators[j];
+                var destElev = elev.get_destination_floor();
+                var reqFloor = requests[i];
+
+                if ((Math.abs(destElev - reqFloor) < Math.abs(nearestElev.get_destination_floor() - reqFloor))) {
+                    nearestElev = elev;
+                }
+            }
+            return nearestElev.commit_decision(requests[i]);
         }
     }
 
